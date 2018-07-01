9f3aab31ab2a984c6b0d979d83c851a50627142b
yoshiken <yskn.beer@gmail.com>
2018-07-01 20:08:13 +0900

    :sparkles: add get wikipedia latest version

---
+++ src/wiki-get.py
@@ -0,0 +1,6 @@
+import requests
+import json
+
+##Get latest ID
+r = requests.get('https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&titles=%E6%A4%8D%E7%94%B0%E3%81%B2%E3%81%8B%E3%82%8B').json()
+print(r['query']['pages']['3589550']['revisions'][0]['revid'])
