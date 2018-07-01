397c6a4d51371710b98d5d5646f1614af7da2ac3
yoshiken <yskn.beer@gmail.com>
2018-07-02 07:29:59 +0900

    :shirt: apply existing code flake

--- src/tw-get.py
+++ src/tw-get.py
@@ -1,14 +1,9 @@
 import twitter
 import read_env as env
 
+print(env.tw_token)
 
-print (env.tw_token)
-
-auth = twitter.OAuth(token = env.tw_token,
-                     token_secret = env.tw_token_secret,
-                     consumer_key = env.tw_consumer_key,
-                     consumer_secret = env.tw_consumer_secret
-                     )
+auth = twitter.OAuth(token=env.tw_token, token_secret=env.tw_token_secret, consumer_key=env.tw_consumer_key, consumer_secret=env.tw_consumer_secret)
 
 t = twitter.Twitter(auth=auth)
 
