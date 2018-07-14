dc5788b78794740ea6cc5df64c0b9ed872bf5105
yoshiken <yskn.beer@gmail.com>
2018-07-14 18:10:28 +0900

    :up: move .env

--- src/API/internal/tw-get.py
+++ src/API/internal/tw-get.py
@@ -1,7 +1,6 @@
 import twitter
 import read_env as env
 
-print(env.tw_token)
 
 auth = twitter.OAuth(token=env.tw_token, token_secret=env.tw_token_secret, consumer_key=env.tw_consumer_key, consumer_secret=env.tw_consumer_secret)
 
