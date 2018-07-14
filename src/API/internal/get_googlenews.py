1a2f25d6ffa5ffc39f3aedbebd14fcffcddbea87
yoshiken <yskn.beer@gmail.com>
2018-07-14 18:25:51 +0900

    :tada: Get Google news

---
+++ src/API/internal/get_googlenews.py
@@ -0,0 +1,19 @@
+import urllib.parse
+import urllib.request
+import json
+import read_env as env
+
+
+def dataGet():
+    url = 'https://newsapi.org/v2/everything?'
+    param = {
+        'q': '植田ひかる',
+        'apiKey': env.news_apikey
+    }
+    paramStr = urllib.parse.urlencode(param)
+    readObj = urllib.request.urlopen(url + paramStr)
+    response = readObj.read()
+    return response
+
+
+data = json.loads(dataGet())
