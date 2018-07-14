b46be9c4e650d115b4d6afce5b2b6ab591a8e9d0
yoshiken <yskn.beer@gmail.com>
2018-07-14 22:37:00 +0900

    :+1: move file and get text

--- src/API/internal/get_wiki.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-# Get latest ID
-r = requests.get('https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&titles=%E6%A4%8D%E7%94%B0%E3%81%B2%E3%81%8B%E3%82%8B').json()
-print(r['query']['pages']['3589550']['revisions'][0]['revid'])
---
+++ src/API/internal/get_wikipedia.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def getWikipedeiaLastId():
+    # Get latest ID
+    res = requests.get('https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&titles=%E6%A4%8D%E7%94%B0%E3%81%B2%E3%81%8B%E3%82%8B').json()
+    # 3589550は植田ひかるさんのページの固有IDっぽい？
+    return (res['query']['pages']['3589550']['revisions'][0]['revid'])
+
+
+def getWikipediaContent():
+    # Get Wikideia Text
+    res = requests.get('https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&titles=%E6%A4%8D%E7%94%B0%E3%81%B2%E3%81%8B%E3%82%8B&rvprop=content').json()
+    print(res['query']['pages']['3589550']['revisions'][0]['*'])
