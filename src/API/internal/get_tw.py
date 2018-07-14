9d257ccf580bf227d29cca3e24c6006cfd0911e8
yoshiken <yskn.beer@gmail.com>
2018-07-14 22:15:44 +0900

    :+1: Get Tweets json

--- src/API/internal/get_tw.py
+++ src/API/internal/get_tw.py
@@ -1,9 +1,13 @@
-import twitter
+import json
+from requests_oauthlib import OAuth1Session
 import read_env as env
 
 
-auth = twitter.OAuth(token=env.tw_token, token_secret=env.tw_token_secret, consumer_key=env.tw_consumer_key, consumer_secret=env.tw_consumer_secret)
-
-t = twitter.Twitter(auth=auth)
-
-tweets = t.statuses.user_timeline(screen_name="uedakousiki")
+def getTweets():
+    twitter = OAuth1Session(env.tw_consumer_key, env.tw_consumer_secret, env.tw_token, env.tw_token_secret)
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+    params = {
+        "screen_name": "uedakousiki"
+    }
+    res = twitter.get(url, params=params)
+    return json.loads(res.text)
