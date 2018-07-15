b46d37e41c2211fce6bc7b56112c2b859ef47a91
yoshiken <yskn.beer@gmail.com>
2018-07-16 02:35:40 +0900

    :recycle: Tweet Class make

--- src/API/internal/get_tw.py
+++ src/API/internal/get_tw.py
@@ -2,12 +2,12 @@ import json
 from requests_oauthlib import OAuth1Session
 import get_env as env
 
-
-def getTweets():
-    twitter = OAuth1Session(env.tw_consumer_key, env.tw_consumer_secret, env.tw_token, env.tw_token_secret)
-    url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-    params = {
-        "screen_name": "uedakousiki"
-    }
-    res = twitter.get(url, params=params)
-    return json.loads(res.text)
+class Tweet:
+    def getTweets(self):
+        twitter = OAuth1Session(env.tw_consumer_key, env.tw_consumer_secret, env.tw_token, env.tw_token_secret)
+        url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+        params = {
+            "screen_name": "uedakousiki"
+        }
+        res = twitter.get(url, params=params)
+        return json.loads(res.text)
