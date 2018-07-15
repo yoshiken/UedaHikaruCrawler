2d90e684d7f7c968dc2560e1ccc820c6e4065747
yoshiken <yskn.beer@gmail.com>
2018-07-15 13:31:15 +0900

    :recycle: fix flake8 codeing

--- src/API/internal/get_eventernote.py
+++ src/API/internal/get_eventernote.py
@@ -1,6 +1,7 @@
 from pyquery import PyQuery as pq
 import re
 
+
 def __getEventernoteHTML():
     return pq(url='https://www.eventernote.com/actors/9735/events?limit=1000000')
 
@@ -19,31 +20,30 @@ def getEventTitle(res, eventcount):
     return title
 
 
-def getEventTime(res,eventcount):
+def getEventTime(res, eventcount):
     timetext = res('body > div.container > div > div.span8.page > div.gb_event_list.clearfix')('.place').text()
     timepattern = r'\d{2}:\d{2}|-'
     alltimedata = re.findall(timepattern, timetext)
     doortime = []
-    showtime =[]
-    closetime =[]
-    for ec in range(eventcount*3):
-        if(ec == 0 or ec%3 ==0):
+    showtime = []
+    closetime = []
+    for ec in range(eventcount * 3):
+        if(ec == 0 or ec % 3 == 0):
             doortime.append(alltimedata[ec])
-        elif(ec%3 == 1):
+        elif(ec % 3 == 1):
             showtime.append(alltimedata[ec])
         else:
             closetime.append(alltimedata[ec])
     return doortime, showtime, closetime
 
 
-def getLocation(res,eventcount):
+def getLocation(res, eventcount):
     location = []
     for ec in range(eventcount):
         locationtext = (res('body > div.container > div > div.span8.page > div.gb_event_list.clearfix > ul > li:nth-child(' + str(ec) + ') > div.event > div:nth-child(2)').text())
         location.append(locationtext.replace('会場: ', ''))
     del location[0]
     return location
-
 
 
 def getEventinfo():
