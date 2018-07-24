9e8928881ad71285ef93f6e66efc5dabc1101b25
yoshiken <yskn.beer@gmail.com>
2018-07-24 19:26:44 +0900

    :sparkles: DBのコネクション周りだけ扱うようにした

--- src/cron/insertDB.py
+++ src/cron/insertDB.py
@@ -1,43 +1,17 @@
-import os
 import psycopg2
-from get_eventernote import Event
 
 
-def get_connection():
-    dsn = 'postgresql://ueda:hikaru@localhost:5432/ueda'
-    conn = psycopg2.connect(dsn)
-    cur = conn.cursor()
-    return cur ,conn
+class connectionsql:
+    @classmethod
+    def openConnection(self):
+        dsn = 'postgresql://ueda:hikaru@postgres:5432/ueda'
+        conn = psycopg2.connect(dsn)
+        cur = conn.cursor()
+        return cur, conn
 
-
-def is_event(title):
-    cur.execute('SELECT * FROM event WHERE title = %s',(title, ))
-    rows = cur.fetchone()
-    return bool(rows)
-
-def insert_event(event):
-    cur.execute('INSERT INTO event (day, title, location) VALUES (%s, %s, %s);', (event['date'], event['title'], event['location']))
-
-def update_event(event):
-    if event['doortime'] != '-':
-        cur.execute('UPDATE event SET doortime = %s WHERE title = %s;', (event['doortime'], event['title']))
-    if event['showtime'] != '-':
-        cur.execute('UPDATE event SET showtime = %s WHERE title = %s;', (event['showtime'], event['title']))
-    if event['closetime'] != '-':
-        cur.execute('UPDATE event SET closetime = %s WHERE title = %s;', (event['closetime'], event['title']))
-
-def event():
-    Events = Event()
-    eventList = Events.getEvents()
-    for event in eventList:
-        if not is_event(event['title']) :
-            insert_event(event)
-        update_event(event)
-    conn.commit()
-
-def is_news(news)
-
-cur, conn = get_connection()
-event()
-cur.close()
-conn.close()
+    @classmethod
+    def closeConnection(self, cur, conn):
+        conn.commit()
+        cur.close()
+        conn.close()
+        return
