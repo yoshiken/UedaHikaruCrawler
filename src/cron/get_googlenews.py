59200e41cb9416c460fed7885e79c77febc75c09
yoshiken <yskn.beer@gmail.com>
2018-07-24 20:10:31 +0900

    :sparkles: db insert news

--- src/cron/get_googlenews.py
+++ src/cron/get_googlenews.py
@@ -1,25 +1,38 @@
 import feedparser
 from pyquery import PyQuery as pq
-import datetime
+from insertDB import connectionsql
+import dateutil.parser
 
 
 class News:
-    def __getNewsHTML(self):
-        url = 'https://news.google.com/news/rss/search/section/q/%E6%A4%8D%E7%94%B0%E3%81%B2%E3%81%8B%E3%82%8B/%E6%A4%8D%E7%94%B0%E3%81%B2%E3%81%8B%E3%82%8B?ned=jp&gl=JP&hl=ja'
-        return feedparser.parse(url)
+    def __init__(self):
+        self.res = feedparser.parse('https://news.google.com/news/rss/search/section/q/%E6%A4%8D%E7%94%B0%E3%81%B2%E3%81%8B%E3%82%8B/%E6%A4%8D%E7%94%B0%E3%81%B2%E3%81%8B%E3%82%8B?ned=jp&gl=JP&hl=ja')
 
-    def __getNewsImageURL(self, res):
-        newstext = pq(res)
+    def __getNewsImageURL(self):
+        newstext = pq(self.res['entries'][0]['summary'])
         newsimg = newstext('img')
         return newsimg.attr('src')
 
-    def __getNewsDate(self, res):
-        return datetime.datetime(*res[:6])
-
     def getNews(self):
-        res = self.__getNewsHTML()
-        title = {'title': res['entries'][0]['title']}
-        link = {'link': res['entries'][0]['link']}
-        imgURL = {'imgURL': self.__getNewsImageURL(res['entries'][0]['summary'])}
-        updatetime = {'updatetime': self.__getNewsDate(res['entries'][0]['published_parsed'])}
-        return title, link, imgURL, updatetime
+        news = {}
+        news['title'] = self.res['entries'][0]['title']
+        news['link'] = self.res['entries'][0]['link']
+        news['imgurl'] = self.__getNewsImageURL()
+        news['updatetime'] = dateutil.parser.parse(self.res['entries'][0]['published'])
+        return news
+
+    def update_news(self, title, link, imgurl, updatetime, cur):
+        cur.execute('SELECT * FROM news WHERE title = %s AND link = %s', (title, link))
+        if cur.fetchone():
+            return
+        else:
+            cur.execute('INSERT INTO news (title, link, imgurl, updatetime) VALUES (%s,%s,%s,%s);', (title, link, imgurl, updatetime))
+
+    def news(self):
+        cur, conn = connectionsql.openConnection()
+        newsinfo = self.getNews()
+        self.update_news(newsinfo['title'], newsinfo['link'], newsinfo['imgurl'], newsinfo['updatetime'], cur)
+        connectionsql.closeConnection(cur, conn)
+
+
+News().news()
