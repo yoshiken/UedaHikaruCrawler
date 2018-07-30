cf2f8577832cd26d9295ff2b238018c0636a8003
yoshiken <yskn.beer@gmail.com>
2018-07-31 07:13:38 +0900

    :sparkles: add templeat

--- src/main.py
+++ src/main.py
@@ -2,13 +2,12 @@ from sanic import Sanic
 from sanic.response import json
 from app import read_db
 
-app = Sanic()
-Events, News = read_db.readInfo().readAll()
-
 
 @app.route("/")
-async def test(request):
-    return json({"hello": "world"})
+async def index(request):
+    app = Sanic()
+    Events, News = read_db.readInfo().readAll()
+    return uri_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
