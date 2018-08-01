8af27396665945c80c2bbf2181e3a067e899ff5b
yoshiken <yskn.beer@gmail.com>
2018-08-02 00:42:17 +0900

    :+1: hello word tpl on jinja in sonic

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic import response
+from jinja2 import Environment, FileSystemLoader
 from app import read_db
 
+
+env = Environment(loader=FileSystemLoader('/app/src/templates/', encoding='utf8'))
+app = Sanic()
 
 @app.route("/")
 async def index(request):
     app = Sanic()
     Events, News = read_db.readInfo().readAll()
-    return uri_template('index.html')
+    tpl = env.get_template('index.html')
+    html = tpl.render({'hoge':'hello'})
+    return response.html(html)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
