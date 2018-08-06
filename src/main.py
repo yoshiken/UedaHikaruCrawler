eb0c2f1b9acaa6492168a3062417709f858d430a
yoshiken <yskn.beer@gmail.com>
2018-08-06 23:54:03 +0900

    :shower: del jinja

--- src/main.py
+++ src/main.py
@@ -2,19 +2,14 @@
 
 from sanic import Sanic
 from sanic import response
-from jinja2 import Environment, FileSystemLoader
 from app import read_db
 
-env = Environment(loader=FileSystemLoader('/app/src/templates/', encoding='utf8'))
 app = Sanic()
 
 @app.route("/")
 async def index(request):
     app = Sanic()
     Events, News = read_db.readInfo().readAll()
-    tpl = env.get_template('index.html')
-    html = tpl.render({'hoge':'hello'})
-    return response.html(html)
 
 # TODO ちゃんとjsonで返したい
 @app.route("/api/events")
