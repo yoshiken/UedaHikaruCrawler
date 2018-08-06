72f02226f9f0b8380bc8686ad98e41050f0506a8
yoshiken <yskn.beer@gmail.com>
2018-08-07 00:48:22 +0900

    :+1: return vuejs html

--- src/main.py
+++ src/main.py
@@ -4,17 +4,20 @@ from sanic import Sanic
 from sanic import response
 from app import read_db
 
-app = Sanic()
+app = Sanic(__name__)
+app.static('/static', './www/static')
 
 @app.route("/")
+@app.route('/<path:path>')
 async def index(request):
-    app = Sanic()
+    fin=open('./www/index.html')
+    html=fin.read()
     Events, News = read_db.readInfo().readAll()
+    return response.html(html)
 
 # TODO ちゃんとjsonで返したい
 @app.route("/api/events")
 async def apiEvents(request):
-    app = Sanic()
     Events = read_db.readInfo().readEvent()
     return response.text(Events)
 
