c452f574593541c1bfef5c755a7e52f3b1df0d1e
yoshiken <yskn.beer@gmail.com>
2018-08-07 23:38:41 +0900

    Revert ":up: install VueMaterial"
    This reverts commit 752b54ecf26ab8cfba534b5aef629a3278f31b4d.

--- src/vue/src/main.js
+++ src/vue/src/main.js
@@ -3,9 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VueMaterial from 'vue-material'
 
-Vue.use(VueMaterial)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
