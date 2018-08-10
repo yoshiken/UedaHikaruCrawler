f560e1f10e7ef9007f396eba70c2428f9e7e9ff9
yoshiken <yskn.beer@gmail.com>
2018-08-10 20:39:16 +0900

    :up: restart vue.js

---
+++ src/vue/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: true,
+  baseUrl: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
