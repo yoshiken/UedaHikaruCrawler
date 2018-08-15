f5ff2ae896d995fbb3a0e363398d30524005f461
yoshiken <yskn.beer@gmail.com>
2018-08-16 06:33:14 +0900

    :shower: Initialization page

--- src/main.py
+++ src/main.py
@@ -9,9 +9,35 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.config["JSON_SORT_KEYS"] = False
 
+app.config['DEBUG'] = True
+
 @app.route('/')
 def catch_all():
+    Events = read_db.readInfo().readEvent()
+    News = read_db.readInfo().readNews()
     return render_template("index.html")
+
+@app.route('/events')
+def catch_event():
+    Events = read_db.readInfo().readEvent()
+    return render_template("events.html")
+
+@app.route('/news')
+def catch_news():
+    Events = read_db.readInfo().readEvent()
+    return render_template("news.html")
+
+@app.route('/twitter')
+def catch_twitter():
+    return render_template("twitter.html")
+
+@app.route('/about')
+def catch_about():
+    return render_template("about.html")
+
+@app.route('/develop')
+def catch_develop():
+    return render_template("develop.html")
 
 @app.route('/api/events')
 def catch_apiEvents():
