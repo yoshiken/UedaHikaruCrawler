914a4fbf547bba0828075a6be420c7c346f0f788
yoshiken <yskn.beer@gmail.com>
2018-08-16 20:01:00 +0900

    :recycle:

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify
 from app import read_db
 
 app = Flask(__name__)
 
-## JSON日本語文字化け対策
+# JSON日本語文字化け対策
 app.config['JSON_AS_ASCII'] = False
 app.config["JSON_SORT_KEYS"] = False
 
 app.config['DEBUG'] = True
+
 
 @app.route('/')
 def catch_all():
@@ -17,43 +18,51 @@ def catch_all():
     News = read_db.readInfo().readNews()
     return render_template("index.html")
 
+
 @app.route('/events')
 def catch_event():
     Events = read_db.readInfo().readEvent()
     return render_template("events.html")
+
 
 @app.route('/news')
 def catch_news():
     Events = read_db.readInfo().readEvent()
     return render_template("news.html")
 
+
 @app.route('/twitter')
 def catch_twitter():
     return render_template("twitter.html")
+
 
 @app.route('/about')
 def catch_about():
     return render_template("about.html")
 
+
 @app.route('/develop')
 def catch_develop():
     return render_template("develop.html")
+
 
 @app.route('/api/events')
 def catch_apiEvents():
     Events = read_db.readInfo().readEvent()
     return jsonify({
-        'status':'200',
-        'events':Events
+        'status': '200',
+        'events': Events
     })
+
 
 @app.route('/api/news')
 def catch_apiNews():
     News = read_db.readInfo().readNews()
     return jsonify({
-        'status':'200',
-        'news':News
+        'status': '200',
+        'news': News
     })
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
