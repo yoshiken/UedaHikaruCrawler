0e3dcae267ebc9b5158c1116c4eb5aa19c54d131
yoshiken <yskn.beer@gmail.com>
2018-09-22 13:00:52 +0900

    :+1: api serverにsanic導入

---
+++ docker/api/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Waiting for Posgresql"
+until psql &> /dev/null
+do
+        >$2 echo -n "."
+        sleep 1
+done
+
+>&2 echo "Posgresql is up - executing command"
+
+echo "start api"
+
+uwsgi --ini /uwsgi.ini
